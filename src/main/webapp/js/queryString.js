ba9533c9f307ceab91231354b45afc529a8fa1db
github school7341 <github school7341@naver.com>
2016-09-20 13:32:43 +0900

    6th Commit : 20160920 : How to Use URL

---
+++ src/main/webapp/js/queryString.js
@@ -0,0 +1,6 @@
+﻿//모듈 추출
+var url=require('url');
+var querystring=require('querystring');
+//모듈 사용
+var parsedObject = url.parse('http://www.naver.com?cp=TEST');
+console.log(querystring.parse(parsedObject.query));
---
+++ src/main/webapp/js/url.js
@@ -0,0 +1,5 @@
+﻿//모듈 추출
+var url=require('url');
+//모듈 사용
+var parsedObject = url.parse('http://www.naver.com/view/test.do?cp=test',true);
+console.log(parsedObject);
