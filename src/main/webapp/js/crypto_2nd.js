1d7f5e21b4c3ea12c292cfc2a5159695061e7153
github school7341 <github school7341@naver.com>
2016-09-20 14:11:18 +0900

    8th Commit : 20160920 : Cryptonism & DeCryptonism

---
+++ src/main/webapp/js/crypto_2nd.js
@@ -0,0 +1,17 @@
+﻿//모듈 추출
+var crypto=require('crypto');
+//변수 선언
+var key='Nobody know my pw';//키 값
+var input = '1234';//암호
+//암호화
+var cipher = crypto.createCipher('aes192',key);
+cipher.update(input,'utf8','base64');
+var cipheredOutput = cipher.final('base64');
+//암호화 해제
+var decipher = crypto.createDecipher('aes192',key);
+decipher.update(cipheredOutput,'base64','utf8');
+var decipheredOutput = decipher.final('utf8');
+//출력
+console.log('Original PW : '+ input);
+console.log('Cryptism PW : '+ cipheredOutput);
+console.log('DeCryptism PW : '+ decipheredOutput);
