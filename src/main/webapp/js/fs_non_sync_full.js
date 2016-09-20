963ef539b1f41f595a8b740f8f180e439e7180e9
github school7341 <github school7341@naver.com>
2016-09-20 14:56:31 +0900

    13th Commit : 20160920 : Handle the Exception by Non-Syncronized Method

---
+++ src/main/webapp/js/fs_non_sync_full.js
@@ -0,0 +1,20 @@
+﻿//모듈 추출
+var fs=require('fs');
+//비동기식-FileRead
+
+fs.readFile('textfile.txt','utf8',function(error,data){
+	if(error){
+		console.log(error);
+	} else {
+		console.log(data);
+	}
+});
+
+//비동기식-FileWrite
+fs.writeFile('textfile.txt','Wow, another method!!!','utf8', function(error){
+if(error){
+	console.log(error);
+} else {
+	console.log("Write File Non-Sync Complete.");
+}
+});
