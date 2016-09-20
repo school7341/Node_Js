853040f400e35b23d28b1a57a2d8a6fd13ff8827
github school7341 <github school7341@naver.com>
2016-09-20 14:33:52 +0900

    11th Commit : 20160920 : Filewrite

---
+++ src/main/webapp/js/fs_write.js
@@ -0,0 +1,12 @@
+﻿//모듈 추출
+var fs=require('fs');
+//변수 선언
+var data = 'Hello Node Js...!!';
+//모듈 사용
+//동기식
+var text=fs.writeFileSync('TextFileOtherWriteSync.txt',data,'utf8');
+console.log("Write File Sync Complete.");
+//비동기식
+fs.writeFile('TextFileOtherWrite.txt',data,'utf8', function(error){
+console.log("Write File Non-Sync Complete.");
+});
