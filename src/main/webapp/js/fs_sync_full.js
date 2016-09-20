db123a05cdd331d42bc41a17ee7087d50c74e4ed
github school7341 <github school7341@naver.com>
2016-09-20 14:43:24 +0900

    12th Commit : 20160920 : Handle the Exception by Syncronized Method

---
+++ src/main/webapp/js/fs_sync_full.js
@@ -0,0 +1,16 @@
+﻿//모듈 추출
+var fs=require('fs');
+//모듈 사용-FileRead
+try{
+	var text=fs.readFileSync('textfile.txt','utf8');
+	console.log(text);
+} catch(e) {
+	console.log(e);
+}
+//모듈 사용-FileWrite
+try{
+	var text=fs.writeFileSync('textfile.txt','Input Text Here.','utf8');
+	console.log("Write File Sync Complete.");
+} catch(e) {
+	console.log(e);
+}
