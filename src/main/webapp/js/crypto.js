ef1575690e5374d897c952b1149a747bb33dedc8
github school7341 <github school7341@naver.com>
2016-09-20 13:34:10 +0900

    7th Commit : 20160920 : Cryptonism 1st

---
+++ src/main/webapp/js/crypto.js
@@ -0,0 +1,8 @@
+﻿//모듈 추출
+var crypto=require('crypto');
+//해쉬 생성
+var shasum = crypto.createHash('sha1');
+shasum.update('Seokhyeon Seo');//변경하고자하는 문자열
+var output=shasum.digest('hex');
+//출력
+console.log("SeoS' Name_hash : ", output);
